1f00f1f00446341a6cf2936134cdc86a985f34cf
furyu <furyutei@gmail.com>
2021-06-27 18:10:53 +0900

    0.3.2.24: 近傍検索でスクロールが開始しない不具合の改善
    - ユーザータイムラインの近傍検索でスクロールが開始しないことがある不具合改善の試み

--- src/scripts/feature_switch.js
+++ src/scripts/feature_switch.js
@@ -1,32 +1,43 @@
 ( () => {
-    'use strict';
-    
-    const
-        DEBUG = document.documentElement.dataset.twdvDebug,
-        featureSwitch = window.__INITIAL_STATE__?.featureSwitch;
-    
-    if ( DEBUG ) {
-        console.debug( '*** START PATCH ***', featureSwitch );
-        console.debug( 'window.__INITIAL_STATE__ = ', JSON.stringify( window.__INITIAL_STATE__, null, 4 ) );
-    }
-    if ( ! featureSwitch ) {
-        console.error( '[twDisplayVicinity] featureSwitch not found' );
-        return;
-    }
-    
-    try {
-        if ( DEBUG ) console.debug( 'featureSwitch.defaultConfig.responsive_web_graphql_profile_timeline.value:', featureSwitch.defaultConfig.responsive_web_graphql_profile_timeline.value, '=>', false );
-        featureSwitch.defaultConfig.responsive_web_graphql_profile_timeline.value = false;
-    }
-    catch ( error ) {
-        console.error( '[twDisplayVicinity]', error );
-    }
-    
-    try {
-        if ( DEBUG ) console.debug( 'featureSwitch.user.config.responsive_web_graphql_profile_timeline.value:', featureSwitch.user.config.responsive_web_graphql_profile_timeline.value, '=>', false );
-        featureSwitch.user.config.responsive_web_graphql_profile_timeline.value = false;
-    }
-    catch ( error ) {
-        console.error( '[twDisplayVicinity]', error );
-    }
+'use strict';
+
+const
+    DEBUG = document.documentElement.dataset.twdvDebug,
+    observer = new MutationObserver( ( records ) => {
+        if ( ! window.__INITIAL_STATE__ ) {
+            return;
+        }
+        
+        observer.disconnect();
+        
+        const featureSwitch = window.__INITIAL_STATE__.featureSwitch;
+        
+        if ( DEBUG ) {
+            console.debug( '*** START PATCH ***', featureSwitch );
+            console.debug( 'window.__INITIAL_STATE__ = ', JSON.stringify( window.__INITIAL_STATE__, null, 4 ) );
+        }
+        
+        if ( ! featureSwitch ) {
+            console.error( '[twDisplayVicinity] featureSwitch not found' );
+            return;
+        }
+        
+        try {
+            if ( DEBUG ) console.debug( 'featureSwitch.defaultConfig.responsive_web_graphql_profile_timeline.value:', featureSwitch.defaultConfig.responsive_web_graphql_profile_timeline.value, '=>', false );
+            featureSwitch.defaultConfig.responsive_web_graphql_profile_timeline.value = false;
+        }
+        catch ( error ) {
+            console.error( '[twDisplayVicinity]', error );
+        }
+        
+        try {
+            if ( DEBUG ) console.debug( 'featureSwitch.user.config.responsive_web_graphql_profile_timeline.value:', featureSwitch.user.config.responsive_web_graphql_profile_timeline.value, '=>', false );
+            featureSwitch.user.config.responsive_web_graphql_profile_timeline.value = false;
+        }
+        catch ( error ) {
+            console.error( '[twDisplayVicinity]', error );
+        }
+    } );
+
+observer.observe( document, { childList: true, subtree: true } );
 } )();
